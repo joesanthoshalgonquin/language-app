0461764a0b57b29f7326b327cecabcb4337911d2
joesanthoshalgonquin <sant0232@algonquinlive.com>
2024-03-27 12:37:39 -0400

    Added React Router and added a root route

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import Navbar from './components/navbar.component'
 import HomePage from './pages/home/home.component'
+
+import './App.css'
 
 function App() {
   return (
     <>
-      <HomePage />
+      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
